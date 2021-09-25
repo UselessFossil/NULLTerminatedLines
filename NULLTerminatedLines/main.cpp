1b8febe8c8f3e9283a6a2e5b0a5d242a371dbd62
georgeyatsuk@yandex.ru <georgeyatsuk@yandex.ru>
2021-09-25 03:03:23 +0300

    NULL Terminated Lines in progress

--- NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int StrLen(char str[]);
+void to_upper(char str[]);
+void to_lower(char str[]);
+void shrink(char str[]);
+bool is_palindrome(char str[]);
+void remove_symbol(char str[], char symbol);
+
 
 void main()
 {
@@ -14,13 +20,23 @@ void main()
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	const int n = 20;
-	char str[n];
+	const int n = 256;
+	char str[n] = "јргентина манит негра";
 	cout << "¬ведите строку: ";
-	//cin >> str;
-	cin.getline(str, n);
+	//cin.getline(str, n);
 	cout << str << endl;
-	cout << "ƒлина введЄнной строки: " << StrLen(str) << endl;
+	cout << is_palindrome(str) << endl;
+	cout << str << endl;
+	//cout << "ƒлина введЄнной строки: " << StrLen(str) << endl;
+
+	//to_upper(str);
+	//cout << str << endl;
+	//to_lower(str);
+	//cout << str << endl;
+
+	//shrink(str);
+	//cout << str << endl;
+
 }
 
 int StrLen(char str[])
@@ -28,4 +44,70 @@ int StrLen(char str[])
 	int i = 0;
 	for (; str[i]; i++);
 	return i;
+}
+
+void to_upper(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		//≈—Ћ» элемент строки str[i] - ћјЋ≈Ќ№ јя Ѕ” ¬ј
+		if (
+			str[i] >= 'a' && str[i] <= 'z' ||
+			str[i] >= 'а' && str[i] <= '€'
+			)
+			str[i] -= 32; // тогда преобразуем его в большую букву
+	}
+}
+
+void to_lower(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (
+			str[i] >= 'A' && str[i] <= 'Z' ||
+			str[i] >= 'ј' && str[i] <= 'я'
+			)
+			str[i] += 32;
+	}
+}
+
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while(str[i] == ' ' && str[i+1] == ' ')
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+
+void remove_symbol(char str[], char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+
+bool is_palindrome(char str[])
+{
+	char* buffer = new char[StrLen(str) + 1]{};
+	for (int i = 0; str[i]; i++)buffer[i] = str[i];
+	remove_symbol(buffer, ' ');
+	int n = StrLen(buffer);
+	to_lower(buffer);
+	for (int i = 0; i < n / 2; i++)
+	{
+		if (buffer[i] != buffer[n - 1 - i])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[] buffer;
+	return true;
 }
